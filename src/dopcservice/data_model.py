f97f24e1923905b66eca8663bab22d643989e6f4
Song Meo <songmeo@pm.me>
2025-01-19 15:35:48 +0200

    great arc distance

--- src/dopcservice/data_model.py
+++ src/dopcservice/data_model.py
@@ -2,9 +2,9 @@
 
 from __future__ import annotations
 
-import math
 from dataclasses import dataclass
 from typing import List
+from math import radians, sin, cos, sqrt, atan2
 
 
 @dataclass(frozen=True)
@@ -21,11 +21,16 @@ class GeoLocation:
     lat: float
     lon: float
 
-    def delivery_distance(self, other: GeoLocation) -> float:
-        """Returns the straight line distance in meters"""
-        x1, y1 = self.lat, self.lon
-        x2, y2 = other
-        return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+    def great_arc_distance(self, other: GeoLocation) -> float:
+        # radius of the Earth (default is 637100 meters)
+        radius = 637100
+        # Convert latitude and longitude from degrees to radians
+        lat1, lon1, lat2, lon2 = map(radians, [self.lat, self.lon, other.lat, other.lon])
+        # Differences in coordinates
+        delta_lat, delta_lon = lat2 - lat1, lon2 - lon1
+        # Haversine formula
+        a = sin(delta_lat/2)**2 + cos(lat1) * cos(lat2) * sin(delta_lon/2)**2
+        return 2 * radius * atan2(sqrt(a), sqrt(1-a))
 
 
 @dataclass(frozen=True)
@@ -53,6 +58,7 @@ class DeliveryOrderPrice:
 
 @dataclass(frozen=True)
 class Venue:
+    id: str
     coordinates: GeoLocation
     order_minimum_no_surcharge: Money
     base_price: Money
